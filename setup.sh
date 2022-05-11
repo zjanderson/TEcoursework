aaf3ae3488c8dd59977c265a52f63b3da28395f5
Walt Impellicceiri <walt@techelevator.com>
2022-05-11 00:09:01 +0000

    Update setup.sh for Java Yellow

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@
 # The script uses this value to set it as an upstream for the student.
 # The URL should look something like: "https://git.techelevator.com/campuses/nlr/jan-2022/java-green/instructor-code"
 
-upstream_repo="https://git.techelevator.com/campuses/nlr/may-2022/java-gray/instructor-code.git"
+upstream_repo="https://git.techelevator.com/campuses/nlr/may-2022/java-yellow/instructor-code.git"
 
 echo
 read -r -p "Enter your name (First Last): " name
