d17ea7c6b9e6e988da142dd91ac9392f63bd4d86
Zachary Anderson <anderson.zac@gmail.com>
2022-05-21 12:09:18 -0400

    temp converter works, needs to parse int

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
@@ -6,6 +6,8 @@ public class TempConvert {
 
 	public static void main(String[] args) {
 
+
+
 		Scanner input = new Scanner(System.in);
 
 			System.out.println("Please enter the temperature: ");
@@ -14,8 +16,24 @@ public class TempConvert {
 
 			System.out.println("Is the temperature in (C)elsius, or (F)ahrenheit? ");
 			response = input.nextLine();
+			
+			boolean isCelsiius = (response ==  "C");
 
-		System.out.println();
+			boolean isCelsiius1 = isCelsiius;
+			if (isCelsiius) {
+				int temperatureCelsius = givenTemperature;
+				double temperatureFarenheit = (temperatureCelsius * 1.8) + 32;
+				
+				
+				System.out.println(givenTemperature + "C is " + temperatureFarenheit + "F");
+			} else {
+				int temperatureFarenheit = givenTemperature;
+				double temperatureCelsius = (temperatureFarenheit - 32) / 1.8;
+
+
+			System.out.println(givenTemperature + "F is " + temperatureCelsius + "C");
+			}
+			System.out.println();
 
 	}
 
