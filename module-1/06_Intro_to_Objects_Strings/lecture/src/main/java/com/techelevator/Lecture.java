7c71b4505de9c6a52aa16685671462c89a27d083
Zachary Anderson <anderson.zac@gmail.com>
2022-05-23 12:13:33 -0400

    lecture notes

--- module-1/06_Intro_to_Objects_Strings/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/06_Intro_to_Objects_Strings/lecture/src/main/java/com/techelevator/Lecture.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Locale;
+
 public class Lecture {
 
 	public static void main(String[] args) {
@@ -14,6 +16,9 @@ public class Lecture {
 		 * is not the case for most classes */
 
 		/* create a new instance of String using a literal */
+		System.out.println(); //prints a blank line, for spacing purposes.
+		String heyThere = "Welcome, please proceed.";
+		System.out.println(heyThere);
 
 		
 		System.out.println();
@@ -28,16 +33,95 @@ public class Lecture {
 		 * endsWith
 		 * startsWith
 		 * contains
+		 */
+		String hello = "hello";
+		boolean endswith = hello.endsWith("llo"); //true
+		boolean endswithLLO = hello.endsWith("LLO"); //false
+		boolean startsWith = hello.startsWith("he"); //true
+		if (hello.startsWith("he")) {
+			//since the if-statement is true, any code in here would run
+		}
+
+		boolean hasLL = hello.contains("ll");  //true
+		boolean hasEO = hello.contains("eo"); //false
+		boolean hasEAndO = hello.contains("e") && hello.contains("o"); //true!
+
+		 /*
 		 * indexOf
 		 * lastIndexOf
+		 */
+
+		String helloWorld = "Hello World!";
+		int index = helloWorld.indexOf("o"); //will return 4
+		int indexOfWorld = helloWorld.indexOf("World"); //will return 6
+		int indexOfLastO = helloWorld.lastIndexOf('o'); //will return 7
+		int indexOfHello = helloWorld.indexOf("hello"); //will return -1, which means there is no match in string
+
+		if (helloWorld.indexOf("World") >= 0){ //would basically check if "World" exists
+
+		}
+
+		 /*
 		 * charAt
+		 */
+
+		int[] array = new int[5];
+		int lastInteger = array[4];
+
+		String techElevator = "Tech Elevator";
+		char t = techElevator.charAt(0); // 'T'
+		char charAt5 = techElevator.charAt(5); // 'E'
+		"blah".charAt(2); //'a'
+
+		/*
 		 * length
-		 * substring
+		 */
+
+		String blah = "blah";
+		int lengthBlah = blah.length();  //4
+		int lengthMyString = "Hello Zac".length(); //9
+
+		 /*
+		 * substring  //a way to extract a string from another string
+		 */
+
+		String newGreeting = "Hello Everyone";
+
+		String mySubstring = newGreeting.substring(5); //this returns " Everyone"
+		String myOtherSubstring = newGreeting.substring(6, 8); //this returns "Ev"
+
+		newGreeting.substring(6, newGreeting.length()); //this returns "Everyone"
+
+		int i = 5;
+		String twoLetters = newGreeting.substring(i, i+2); //" E"
+
+		 /*
 		 * toLowerCase
 		 * toUpperCase
-		 * trim
+		 */
+		String  city = "Pittsburgh";
+		String cityUpper = city.toUpperCase(); //"PITTSBURGH"
+		String  cityLower = city.toLowerCase(); //"pittsburgh"
+
+
+		 /*
+		 * trim  //removes white space if there's some at the beginning or end
+		 */
+
+		 String userInput = " Zac Anderson ";
+		 String userInputTrimmed = userInput.trim(); //"Zac Anderson"
+
+		 /*
 		 * replace
-		 *
+		 */
+
+		String stringToSearch = "May 23rd, 2022";
+		String replaced = stringToSearch.replace("2", "0"); //"May 03rd, 0000"
+		String replaceAgain = stringToSearch.replace("May", "June"); //"June 23rd, 2022"
+
+		"1 2 3 4 5".replace(' ', ','); //"1,2,3,4,5"
+
+		 /*
 		 */
 
 
@@ -47,8 +131,16 @@ public class Lecture {
 		System.out.println("**********************");
 		System.out.println();
 
+		String method = "Method";
+		String capMethod = method.toUpperCase(); //"METHOD"
+		if (capMethod.contains("TH")) {
+			//this would execute code in here since it's true
+		}
 
-		
+		if (method.toUpperCase().contains("TH")) {
+			//this chained code will do the same as the above multiple step process, will run this code as well
+		}
+
 		System.out.println();
 		System.out.println("**********************");
 		System.out.println("****** EQUALITY ******");
@@ -84,9 +176,22 @@ public class Lecture {
 	/*
 	 * howManyOf("test", 't') -> 2
 	 * howManyOf("test", 'a') -> 0
+	 *
+	 * str -> "Hello World"
+	 * x -> 'o'
 	 */
 	public static int howManyOf(String str, char x) {
-		return 0;
+		int numCharacters = 0;
+		int i = 0;
+
+		for (i = 0; i < str.length(); i++){
+			if (str.charAt(i) == x) {
+				numCharacters++;
+			}
+		}
+
+
+		return numCharacters;
 	}
 
 	/*
@@ -95,13 +200,44 @@ public class Lecture {
 	 * isPalindrome("blah") -> false
 	 */
 	public static boolean isPalindrome(String word) {
-		return false;
+		if (word.length() == 0) {
+			return true; //since an empty string is technically the same forward and back it's true
+		}
+
+		int i = 0;
+		int j = word.length() - 1; //we need two counters since we're comparing two things
+
+		while (i < j) {  //while loop simplifies this to write and read, when they are equal we have reached middle
+			if (word.charAt(i) != word.charAt(j)){
+				return false; //since every time these paramaters need to be true, if it's false once we can exit loop
+			}
+			i++;
+			j--;
+		}
+
+		return true;
 	}
 
 	/*
 	 * makeWordsTitleCase("THE quick BrowN fox") -> "The Quick Brown Fox"
+	 *
+	 * "THE quick BrowN fox"
+	 * "THE" -> take first character to uppercase + following characters to lowercase yields title case
+	 * we can split the string to an array of strings -> ["THE" "quick" "BrowN" "Fox"]
+	 *
+	 *
 	 */
 	public static String makeWordsTitleCase(String sentence) {
-		return null;
+		String[] words = sentence.split(" "); //creates array of words from the sentence
+		String returnValue = ""; //empty string, this is what we'll return at the end
+
+		for (int i = 0; i < words.length; i++) {
+			String word = words[i];			//looks through the array words[],
+			String newWord = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase(); // 
+			returnValue += newWord + " ";
+
+		}
+
+		return returnValue.trim();
 	}
 }
