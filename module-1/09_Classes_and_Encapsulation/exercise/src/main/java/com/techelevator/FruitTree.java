2ccbc6052a2ac9d024a4191e569af8fc919e9b9d
Zachary Anderson <anderson.zac@gmail.com>
2022-05-26 12:44:50 -0400

    first homework example completed together

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/FruitTree.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/FruitTree.java
@@ -1,5 +1,28 @@
 package com.techelevator;
 
 public class FruitTree {
+    private String typeOfFruit;  //field
+    private int piecesOfFruitLeft;  //field
+
+    public String getTypeOfFruit() { //getter
+        return typeOfFruit;
+    }
+    public int getPiecesOfFruitLeft() { //getter
+        return piecesOfFruitLeft;
+    }
+
+    public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {  //constructors
+        this.typeOfFruit = typeOfFruit;
+        piecesOfFruitLeft = startingPiecesOfFruit;
+    }
+
+    public boolean pickFruit(int numberOfPiecesToRemove) {  //method
+        if (piecesOfFruitLeft < numberOfPiecesToRemove) {
+            return false;
+        }
+
+        piecesOfFruitLeft = piecesOfFruitLeft - numberOfPiecesToRemove;
+        return true;
+    }
 
 }
