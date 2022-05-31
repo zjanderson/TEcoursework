8fd362cc803e533b940de99f572b7a977fe691bf
Zachary Anderson <anderson.zac@gmail.com>
2022-05-30 23:59:34 -0400

    updates

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
@@ -6,9 +6,9 @@ public class Television {
     /*
         Fields, Properties, Member Variables, Instance Variables
      */
-    private boolean isOn; //need getter, Whether or not the TV is turned on.
-    private int currentChannel; //need getter, The value for the current channel. Channel levels go between 3 and 18.
-    private int currentVolume; //need getter, The current volume level.
+    private boolean isOn = false; //need getter, Whether or not the TV is turned on.
+    private int currentChannel = 3; //need getter, The value for the current channel. Channel levels go between 3 and 18.
+    private int currentVolume = 2; //need getter, The current volume level.
 
 
 
@@ -17,12 +17,12 @@ public class Television {
                           However, the instance variables need default values:
                           a new TV is off by default. The channel is set to three
                           and the volume level to two.
-     */
-                 ////NOT SURE if the below is corrext, I'm stuck
+
+                 ////NOT SURE if the below is correct, I'm stuck
     public static final boolean DEFAULT_ON_OFF = false;  //default state is off
     public static final int DEFAULT_CHANNEL = 3;  //default channel
     public static final int DEFAULT_VOLUME = 2; //default volume
-
+ */
 
 
 
@@ -32,7 +32,7 @@ public class Television {
         Getters and Setters
      */
 
-    public boolean getisOn() {
+    public boolean isOn() {
         return isOn;
     }
 
@@ -48,8 +48,13 @@ public class Television {
     /*
         Methods
      */
+    public void turnOff() {
+        isOn = false;
+    }
 
-
-
-
+    public void turnOn() {
+        isOn = true;
+        currentChannel = 3;
+        currentVolume = 2;
+    }
 }
