3ebd38d160678ea862293ffdfc0501f761288410
Walt Impellicceiri <walt@techelevator.com>
2022-06-03 08:52:23 -0400

    M1D15 - lecture code and slides.

---
+++ module-1/15_Unit_Testing/lecture/src/main/java/com/techelevator/fruittree/FruitTree.java
@@ -0,0 +1,28 @@
+package com.techelevator.fruittree;
+
+public class FruitTree {
+    private final String typeOfFruit;
+    private int piecesOfFruitLeft;
+
+    public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
+        this.piecesOfFruitLeft = startingPiecesOfFruit;
+        this.typeOfFruit = typeOfFruit;
+    }
+
+    public String getTypeOfFruit() {
+        return typeOfFruit;
+    }
+
+    public int getPiecesOfFruitLeft() {
+        return piecesOfFruitLeft;
+    }
+
+    public boolean pickFruit(int numberOfPiecesToRemove) {
+        if (piecesOfFruitLeft - numberOfPiecesToRemove >= 0) {
+            piecesOfFruitLeft -= numberOfPiecesToRemove;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
