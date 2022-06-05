72412c6439f0752ef7963511f1d766c02d33da3d
Zachary Anderson <anderson.zac@gmail.com>
2022-06-04 22:04:13 -0400

    struggling with inheritance

--- module-1/14_Managing_Inheritance/exercise/src/main/java/com/techelevator/RectangleWall.java
+++ module-1/14_Managing_Inheritance/exercise/src/main/java/com/techelevator/RectangleWall.java
@@ -2,14 +2,44 @@ package com.techelevator;
 
 public class RectangleWall extends Wall{
     // Fields, Instance Variables
-
+    private int length; //getter
+    private int height; //getter
 
     //Constructors
+    public RectangleWall(String name, String color, int length, int height) {
+        name = getName();
+        color = getColor();
+        this.length = length;
+        this.height = height;
+    }
+    public RectangleWall() {
+        super(getName(), getColor());
+        this.length = length;
+        this.height = height;
+
+    }
 
 
     // Getters and Setters
 
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
 
     // Methods
+    public int getArea() {
+        return (length * height);
+    }
+
+    @Override
+    public String toString() {
+        return getName() + (length * height) +
+                " retangle";
+    }
 
 }
