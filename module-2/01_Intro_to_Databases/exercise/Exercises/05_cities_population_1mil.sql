9ab318eea3bb95a24fc534848ca0c60a05b8ac60
Zachary Anderson <anderson.zac@gmail.com>
2022-06-13 21:02:03 -0400

    13 of 23 exercises completed and passing

--- module-2/01_Intro_to_Databases/exercise/Exercises/05_cities_population_1mil.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/05_cities_population_1mil.sql
@@ -1,2 +1,5 @@
 -- 5. The name, state abbreviation, and population of cities with a population greater than 1,000,000 people (10 rows)
 
+SELECT city_name, population, state_abbreviation FROM city WHERE (population > 1000000)
+
+
--- module-2/01_Intro_to_Databases/exercise/Exercises/06_states_200k_sqkm.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/06_states_200k_sqkm.sql
@@ -1,2 +1,3 @@
 -- 6. The name, abbreviation, population, and area of states with an area greater than 200,000 square kilometers (16 rows)
 
+SELECT state_name, population, state_abbreviation, area FROM state WHERE (area > 200000)
