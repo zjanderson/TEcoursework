192413400265337b2a79e79eb923d13e46cb3e39
Zachary Anderson <anderson.zac@gmail.com>
2022-06-14 09:38:10 -0400

    finish exercise

--- module-2/01_Intro_to_Databases/exercise/Exercises/15_city_cities.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/15_city_cities.sql
@@ -1,2 +1,3 @@
 -- 15. The name, state abbreviation, and population for cities that end with the word "City" (11 rows)
 
+SELECT city_name, population, state_abbreviation FROM city WHERE city_name LIKE '%City'
