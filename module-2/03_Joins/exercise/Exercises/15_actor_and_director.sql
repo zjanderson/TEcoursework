91575eca706c3c8b7f62cb49961f585463251e60
Zachary Anderson <anderson.zac@gmail.com>
2022-06-16 09:41:12 -0400

    exercise reviews

--- module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
+++ module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
@@ -1,2 +1,8 @@
 -- 15. The title of the movie and the name of director for movies where the director was also an actor in the same movie (73 rows)
+-- we need person, movie, movie.actor
 
+SELECT movie.title, person.person_name
+FROM movie
+INNER JOIN movie_actor USING (movie_id)
+INNER JOIN person ON person.person_id = movie.director_id
+                  AND person.person_id = movie_actor.actor_id
--- module-2/03_Joins/exercise/Exercises/21_george_num_movies.sql
+++ module-2/03_Joins/exercise/Exercises/21_george_num_movies.sql
@@ -1,3 +1,13 @@
--- 21. For every person in the person table with the first name of "George", list the number of movies they've been in--include all Georges, even those that have not appeared in any movies. Display the names in alphabetical order. (59 rows)
+-- 21. For every person in the person table with the first name of "George",
+--list the number of movies they've been in--include all Georges,
+--even those that have *not* appeared in any movies.
+--Display the names in alphabetical order. (59 rows)
 -- Name the count column 'num_of_movies'
 
+SELECT person_name, COUNT(movie_id) AS num_of_movies
+FROM person
+LEFT JOIN movie_actor ON person.person_id = movie_Actor.actor_id
+LEFT JOIN movie USING (movie_id)
+WHERE person_name LIKE 'George %'
+GROUP BY person.person_id, person_name --this ensures we don't group mult diff ppl with same name
+ORDER BY person_name;
