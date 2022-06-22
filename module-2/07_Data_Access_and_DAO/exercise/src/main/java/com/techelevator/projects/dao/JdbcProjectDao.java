59fcc5756329e5995acf1421c00980118e4f527b
Zachary Anderson <anderson.zac@gmail.com>
2022-06-22 10:01:23 -0400

    m2d07 exercise work

--- module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcProjectDao.java
+++ module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcProjectDao.java
@@ -1,7 +1,9 @@
 package com.techelevator.projects.dao;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Date;
 
 import javax.sql.DataSource;
 
@@ -20,8 +22,25 @@ public class JdbcProjectDao implements ProjectDao {
 
 	@Override
 	public Project getProject(int projectId) {
-		return new Project(0, "Not Implemented Yet", null, null);
+//		return new Project(0, "Not Implemented Yet", null, null);
+		Project project = null;
+
+		//2. write the query, execute it.
+
+		String sql = "" + "SELECT project_id, name, from_date, to_date FROM project WHERE project_id = ?;";
+
+		SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, projectId);
+
+		//3. process the results
+
+		if (rowSet.next()) {
+			project = mapRowToProject(rowSet);
+		}
+
+		return project;
+
 	}
+
 
 	@Override
 	public List<Project> getAllProjects() {
@@ -37,6 +56,30 @@ public class JdbcProjectDao implements ProjectDao {
 	public void deleteProject(int projectId) {
 
 	}
-	
+
+	private Project mapRowToProject(SqlRowSet rowSet) {
+		Project project = new Project();
+
+		int projectId = rowSet.getInt("project_id");
+		project.setId(projectId);
+
+		String name = rowSet.getString("name");
+		project.setName(name);
+
+		Date fromDate = rowSet.getDate("from_date"); //the date can be null, can't call .operator on null, need to check
+		if (fromDate != null) {
+			LocalDate fromDateLocal = fromDate.toLocalDate();
+			project.setFromDate(fromDateLocal);
+		}
+
+		Date toDate = rowSet.getDate("to_date"); //the date can be null, can't call .operator on null, need to check
+		if (toDate != null) {
+			LocalDate toDateLocal = toDate.toLocalDate();
+			project.setToDate(toDateLocal);
+		}
+
+		return project;
+	}
+
 
 }
