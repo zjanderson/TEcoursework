7fabc349a59c910dafb14d07d02d49795e8dad69
Zachary Anderson <anderson.zac@gmail.com>
2022-06-27 12:15:07 -0400

    error, won't connect

--- module-2/11_Consuming_RESTful_APIs_Part_1/lecture/src/main/java/com/techelevator/hotels/App.java
+++ module-2/11_Consuming_RESTful_APIs_Part_1/lecture/src/main/java/com/techelevator/hotels/App.java
@@ -1,5 +1,7 @@
 package com.techelevator.hotels;
 
+import com.techelevator.hotels.model.Hotel;
+import com.techelevator.hotels.model.Review;
 import com.techelevator.hotels.services.ConsoleService;
 import com.techelevator.hotels.services.HotelService;
 
@@ -19,12 +21,16 @@ public class App {
         while (menuSelection != 0) {
             consoleService.printMainMenu();
             menuSelection = consoleService.promptForMenuSelection();
+
             if (menuSelection == 1) {
-                System.out.println("Not implemented");
+                Hotel[] allHotels = hotelService.listHotels(); //we built the method in hotelservice.java, call it here
+                consoleService.printHotels(allHotels); //feed the hotel[] we just created into the console to print it out
+
             } else if (menuSelection == 2) {
                 System.out.println("Not implemented");
             } else if (menuSelection == 3) {
-                System.out.println("Not implemented");
+                 Review[] reviewsFoHotel1 = hotelService.getReviewsByHotelId(1);
+                 consoleService.printReviews(reviewsFoHotel1);
             } else if (menuSelection == 4) {
                 System.out.println("Not implemented");
             } else if (menuSelection == 5) {
--- module-2/11_Consuming_RESTful_APIs_Part_1/lecture/src/main/java/com/techelevator/hotels/services/HotelService.java
+++ module-2/11_Consuming_RESTful_APIs_Part_1/lecture/src/main/java/com/techelevator/hotels/services/HotelService.java
@@ -4,13 +4,18 @@ import com.techelevator.hotels.model.Hotel;
 import com.techelevator.hotels.model.Review;
 import org.springframework.web.client.RestTemplate;
 
-public class HotelService {
+public class HotelService {  ////the job of this class is to send requests to the server and bring back info to make objects
 
     private static final String API_BASE_URL = "http://localhost:3000/";
     private final RestTemplate restTemplate = new RestTemplate();
 
     public Hotel[] listHotels() {
-        return null;
+
+        String url = API_BASE_URL + "hotels";
+
+        Hotel[] allHotels = restTemplate.getForObject(url, Hotel[].class);  //getForObject is generic, so you have to specify that you expect a hotel array
+
+        return allHotels;
     }
 
     public Review[] listReviews() {
@@ -18,15 +23,26 @@ public class HotelService {
     }
 
     public Hotel getHotelById(int id) {
-        return null;
+        //http://localhost:3000/hotels/:id
+
+        String url = API_BASE_URL + "hotels/" + id;
+
+        return restTemplate.getForObject(url,Hotel.class);
     }
 
     public Review[] getReviewsByHotelId(int hotelID) {
-        return null;
+        //http://localhost:3000/reviews?hoteID=:hotelID
+
+
+        String url = API_BASE_URL + "hotels/" + hotelID + "/reviews";
+        return restTemplate.getForObject(url, Review[].class);
     }
 
     public Hotel[] getHotelsByStarRating(int stars) {
-        return null;
+
+        String url = API_BASE_URL + "hotels?stars/" + stars;
+
+        return restTemplate.getForObject(url, Hotel[].class);
     }
 
 
