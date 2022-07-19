1bda361a56cc617081c06be57a20eac8e484f41a
Walt Impellicceiri <walt@techelevator.com>
2022-07-19 08:55:18 -0400

    M3D7 - slides and lecture code.

---
+++ module-3/07_JavaScript_Functions/lecture/lecture.js
@@ -0,0 +1,186 @@
+/**
+ * All named functions will have the function keyword and
+ * a name followed by parentheses.
+ * 
+ * @returns {number} 1
+ */
+function returnOne() {
+  return 1;
+}
+
+/**
+ * Functions can also take parameters. These are just variables that are filled
+ * in by whoever is calling the function.
+ *
+ * Also, we don't *have* to return anything from the actual function.
+ *
+ * @param {any} value the value to print to the console
+ */
+function printToConsole(value) {
+  console.log(value);
+}
+
+/**
+ * Write a function called multiplyTogether that multiplies two numbers together. But 
+ * what happens if we don't pass a value in? What happens if the value is not a number?
+ *
+ * @param {number} firstParameter the first parameter to multiply
+ * @param {number} secondParameter the second parameter to multiply
+ */
+
+/**
+ * This version makes sure that no parameters are ever missing. If
+ * someone calls this function without parameters, we default the
+ * values to 0. However, it is impossible in JavaScript to prevent
+ * someone from calling this function with data that is not a number.
+ * Call this function multiplyNoUndefined
+ *
+ * @param {number} [firstParameter=0] the first parameter to multiply
+ * @param {number} [secondParameter=0] the second parameter to multiply
+ */
+
+
+ 
+/**
+ * Functions can return earlier before the end of the function. This could be useful
+ * in circumstances where you may not need to perform additional instructions or have to
+ * handle a particular situation.
+ * In this example, if the firstParameter is equal to 0, we return secondParameter times 2.
+ * Observe what's printed to the console in both situations.
+ * 
+ * @param {number} firstParameter the first parameter
+ * @param {number} secondParameter the second parameter
+ */
+function returnBeforeEnd(firstParameter, secondParameter) {
+  console.log("This will always fire.");
+
+  if (firstParameter == 0) {
+    console.log("Returning secondParameter times two.");
+    return secondParameter * 2;
+  }
+
+  //this only runs if firstParameter is NOT 0
+  console.log("Returning firstParameter + secondParameter.");
+  return firstParameter + secondParameter;
+}
+
+/**
+ * Scope is defined as where a variable is available to be used.
+ *
+ * If a variable is declare inside of a block, it will only exist in
+ * that block and any block underneath it. Once the block that the
+ * variable was defined in ends, the variable disappears.
+ */
+function scopeTest() {
+  // This variable will always be in scope in this function
+  let inScopeInScopeTest = true;
+
+  {
+    // this variable lives inside this block and doesn't
+    // exist outside of the block
+    let scopedToBlock = inScopeInScopeTest;
+  }
+
+  // scopedToBlock doesn't exist here so an error will be thrown
+  if (inScopeInScopeTest && scopedToBlock) {
+    console.log("This won't print!");
+  }
+}
+
+function createSentenceFromUser(name, age, listOfQuirks = [], separator = ', ') {
+  let description = `${name} is currently ${age} years old. Their quirks are: `;
+  return description + listOfQuirks.join(separator);
+}
+
+/* var hoisting */
+
+/* arguments object and rest parameter syntax */
+
+/* Named Functions: Regular vs. Arrow */
+
+/* Anonymous functions */
+
+/* Array functions requiring a function as a parameter */
+
+/**
+ * Takes an array and, using the power of anonymous functions, generates
+ * their sum.
+ *
+ * @param {number[]} numbersToSum numbers to add up
+ * @returns {number} sum of all the numbers
+ */
+function sumAllNumbers(numbersToSum) {
+  return numbersToSum.reduce();
+}
+
+/**
+ * Takes an array and returns a new array of only numbers that are
+ * multiples of 3
+ *
+ * @param {number[]} numbersToFilter numbers to filter through
+ * @returns {number[]} a new array with only those numbers that are
+ * multiples of 3
+ */
+function allDivisibleByThree(numbersToFilter) {
+}
+
+/**
+ * Return true if all numbers are divisible by 3
+ *
+ * @param {number[]} numbers numbers to filter through
+ * @returns {boolean} true if all numbers are divisible by 3
+ */
+ function areAllDivisibleByThree(numbers) {
+
+}
+
+
+/**
+ * Return an array of unique numbers. Try using .reduce()
+ * 
+ * @param {number[]} numbers array of numbers
+ * @returns {number[]} an array of unique numbers
+ */
+function returnOneOfEach(numbers) {
+}
+
+/**
+ * Returns the longest string. Try using a traditional approach, and then
+ * try using .reduce().
+ * 
+ * @param {string[]} strings an array of strings
+ */
+function returnLongestLength(strings) {
+}
+
+/**
+ * Takes an array of strings and returns true if any geese are 
+ * masquerading as a duck.
+ *
+ * @param {string[]} ducks array of ducks
+ * @returns {boolean} true if any object is a goose
+ */
+ function duckDuckGoose(ducks) {
+ }
+
+ /**
+ * Takes an array of numbers and formats them as money.
+ *
+ * @param {number[]} numbers array of numbers
+ * @returns {number[]} an array of numbers formatted as currency $x.xx
+ * 
+ * 
+ * [ 1, 5, 25.2 ] -> [ '$1.00', '$5.00', '$25.20' ]
+ */
+function toMoney(numbers) {
+}
+
+/**
+ * Takes an array of numbers, finds the numbers divisible by 3, doubles them,
+ * and returns a new array as a result.
+ * 
+ * @param {number[]} numbers 
+ * @returns {number[]} an array of numbers divisible by 3 doubled
+ */
+function methodChainingExample(numbers) {
+}
