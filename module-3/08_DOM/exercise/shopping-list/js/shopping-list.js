eb7f0c53908a6965781572a07f2d4d478ece63ed
Zachary Anderson <anderson.zac@gmail.com>
2022-07-21 15:44:51 -0400

    at a point in exercise that I'm about to change everything, want to be able to undo

--- module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
+++ module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
@@ -1,24 +1,51 @@
 // add pageTitle
+const pageTitle = 'My Shopping List';
 
-
-// add groceries
+// add groceries -- might need to add 'completed' boolean, syntax , completed: false 
+const groceries = [
+  { id: 1, name: 'Chicken', completed: false},
+  { id: 2, name: 'Lye', completed: false},
+  { id: 3, name: 'Whiskey', completed: false},
+  { id: 4, name: 'Pallet of Cabbage', completed: false},
+  { id: 5, name: 'A Single Grape', completed: false},
+  { id: 6, name: 'Live Salmon', completed: false},
+  { id: 7, name: 'Slingshot', completed: false},
+  { id: 8, name: 'GameStop Franchise', completed: false},
+  { id: 9, name: 'Local Stream Detritus', completed: false},
+  { id: 10, name: 'Flux Capacitor', completed: false}
+];
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  const title = document.getElementById('title');
+  title.innerText = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  const ul = document.getElementById('groceries');
+  groceries.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerText = item.name;
+    ul.appendChild(li);
+  });
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  // const li = document.querySelectorAll('li');
+  groceries.forEach((item) => {
+    item.completed = true;
+  });
+}
 
 setPageTitle();
 
